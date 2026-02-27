b3ff185b6f181b7c30bd7b43909c0dcd859d946c
edithgriesser <156580106+edithgriesser@users.noreply.github.com>
2026-02-27 21:43:07 +0100

    update

--- gw_recharge_app/pages/01_Intro.py
+++ gw_recharge_app/pages/01_Intro.py
@@ -39,7 +39,7 @@ with col2:
     st.map(df, latitude= 'lat', longitude='long', color='#4292C6', height=350)
     st.write(':gray[*Figure 1: Map showing the location of the Meterological Station of the Universitiy of Graz.*]')
 
-data = pd.read_csv('data/UniGraz1990-2020.csv', index_col=0, parse_dates=True)
+data = pd.read_csv('../data/UniGraz1990-2020.csv', index_col=0, parse_dates=True)
 st.write(''':gray[*Table 1: Head of the provided DataFrame, where: T_14...Temperature at 14:00 [°C],
     T_a...average Tamperature [°C],
     rel_h_14...relative humidity messured at 14:00 [%],
